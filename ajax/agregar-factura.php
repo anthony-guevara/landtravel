e295f78fb63efd5d9d379ba0f91197d9e798586c
anthony-guevara <yalidanthony@gmail.com>
2020-07-16 22:43:53 -0600

    FIXED: comprar paquetes

--- ajax/agregar-factura.php
+++ ajax/agregar-factura.php
@@ -1,25 +1,65 @@
 <?php 
 error_reporting(0);
 include_once("../bd/config.php");
-include_once("../bd/connection.php");
+include_once("../bd/conexion_mysqli.php");
 
-$datos=array($_POST['usuario'],$_POST['tarjeta'],$_POST['cvv'],$_POST['fechaexp'],$_POST['nidtour']);
+$datos=array((int)$_POST['usuario'],(int)$_POST['tarjeta'],$_POST['cvv'],$_POST['fechaexp'],(int)$_POST['nidtour']);
 
 for($i=0;$i<count($datos);$i++){
 	if ($datos[$i]==""){
 		$datos[$i]=null;
 	}
+
 };
 
-            $sql_sp = "CALL SP_Compra($datos[0], $datos[1], $datos[2], '$datos[3]', $datos[4], @pmensaje, @pcodigo);";
-			$ins_conexion = new Connection();
-			$ins_conexion->connect();
-            $ins_conexion->setConsulta($sql_sp);
-			$qry = $ins_conexion->getResultConsulta();
-			$return = $ins_conexion->getParametroSP("@pmensaje,@pcodigo");
-			$mensajeSP[0] = $return['@pmensaje'];
-			$mensajeSP[1] = $return['@pcodigo'];
+
+			// echo json_encode($datos);
+
+            // $sql_sp = "CALL SP_Compra($datos[0], $datos[1], $datos[2], '$datos[3]', $datos[4], @pmensaje, @pcodigo);";
+			// $ins_conexion = new Connection();
+			// $ins_conexion->connect();
+            // $ins_conexion->setConsulta($sql_sp);
+			// $qry = $ins_conexion->getResultConsulta();
+			// $return = $ins_conexion->getParametroSP("@pmensaje,@pcodigo");
+			// $mensajeSP[0] = $return['@pmensaje'];
+			// $mensajeSP[1] = $return['@pcodigo'];
+
+		
 			
-			echo json_encode($mensajeSP);
-			mysqli_close($conexion);
+			// echo json_encode($mensajeSP);
+			// mysqli_close($conexion);
+
+
+			$connexionMysqli = new ConnexionMysqli();
+
+			$mysqli =  $connexionMysqli->connect();
+
+			
+			$call = $mysqli->prepare('CALL SP_Compra(?, ? , ? , ? , ? , @pmensaje, @pcodigo);');
+
+			$call->bind_param('iissi',
+			$datos[0],
+			$datos[1],
+			$datos[2],
+			$datos[3],
+			$datos[4],
+			);
+
+			
+			$call->execute();
+					
+			$select = $mysqli->query('SELECT  @pmensaje, @pcodigo');
+					
+			$result = $select->fetch_assoc();
+
+			$codigo = $result['@pcodigo'];
+			$mensaje = $result['@pmensaje'];
+
+			echo json_encode(array(
+				$mensaje,
+				$codigo
+			));
+
+
+			$mysqli -> close();
  ?>
---
+++ procedimientos/sp_compra.sql
@@ -0,0 +1,59 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `landtravel`.`SP_Compra`(IN `nusuario` INT(11), IN `ntarjeta` BIGINT(16), IN `ncvv` VARCHAR(45), IN `nfechaexp` VARCHAR(10), IN `nidtour` INT(11), OUT `pmensaje` VARCHAR(45), OUT `pcodigo` INT)
+SP:BEGIN
+DECLARE ultima_factura INT DEFAULT 0;
+DECLARE ultima_detalle INT DEFAULT 0;
+DECLARE monto float;
+     SET autocommit=0; 
+     START TRANSACTION;
+
+    
+        
+     
+        	IF nusuario = "" or nusuario is null THEN
+				SET pMensaje = "El usuario no existe";
+                set pcodigo=0;
+                	leave sp;
+			END IF;
+                    	IF ntarjeta = "" or ntarjeta is null THEN
+				SET pMensaje = "La tarjeta no existe";
+                     set pcodigo=0;
+                	leave sp;
+			END IF;
+                    	IF ncvv = "" or ncvv is null THEN
+				SET pMensaje = "El cvv no existe";
+                     set pcodigo=0;
+                	leave sp;
+			END IF;
+                    	IF nfechaexp = "" or nfechaexp is null THEN
+				SET pMensaje = "La fecha de expiracion no existe";
+                     set pcodigo=0;
+                	leave sp;
+			END IF;
+					IF nidtour = "" or nidtour is null THEN
+				SET pMensaje = "El paquete no existe";
+                     set pcodigo=0;
+                	leave sp;
+			END IF;
+            
+
+		
+
+ 
+ 	/*Haces un insert a la tabla rutas*/
+	       select max(idfactura)+1 into ultima_factura from factura;
+			select max(idfactura)+1 into ultima_detalle from factura;
+           if(ultima_factura is null || ultima_detalle is null)then
+				set ultima_factura=1;
+                set ultima_detalle=1;
+			end if;
+            
+            select costo into monto from tour where id=nidtour;
+			
+            INSERT INTO factura(idfactura,tipo_pago,usuario_id,tipotarjeta,NumeroTarjeta,CVV,fechaexp) VALUES (ultima_factura,0,nusuario,"Variable",ntarjeta,ncvv,nfechaexp);
+            INSERT INTO detalle_factura(iddetalle_factura,cantidad,monto,factura_idfactura,tour_id) VALUES (ultima_detalle,1,monto,ultima_factura,nidtour);
+	
+
+    set pMensaje="Si crea factura";
+    set pcodigo=1;
+commit;
+END
