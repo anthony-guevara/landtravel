d9ffa564cf35d35b8645b00c22b65a9895707a49
anthony-guevara <yalidanthony@gmail.com>
2020-07-21 10:50:51 -0600

    FIXED: mejorar agregar tour

--- ajax/agregar-tour.php
+++ ajax/agregar-tour.php
@@ -1,9 +1,10 @@
 <?php 
 error_reporting(0);
 include_once("../bd/config.php");
-include_once("../bd/connection.php");
 
-$datos=array($_POST['Nombre'],$_POST['fecha'],$_POST['cupos'],$_POST['tipos']);
+include_once("../bd/conexion_mysqli.php");
+
+$datos=array($_POST['Nombre'],$_POST['fecha'],(int)$_POST['cupos'],$_POST['tipos']);
 
 for($i=0;$i<count($datos);$i++){
 	if ($datos[$i]==""){
@@ -11,22 +12,72 @@ for($i=0;$i<count($datos);$i++){
 	}
 };
 
-            $sql_sp = "call SP_Tours('$datos[0]', '$datos[1]', $datos[2],'$datos[3]', 'agregar', @pcmensaje,@pccodigo,@pcid);";
-			$ins_conexion = new Connection();
-			$ins_conexion->connect();
-            $ins_conexion->setConsulta($sql_sp);
-			$qry = $ins_conexion->getResultConsulta();
-			$return = $ins_conexion->getParametroSP("@pcmensaje,@pccodigo,@pcid");
-			$mensajeSP[0] = $return['@pcmensaje'];
-			$mensajeSP[1] = $return['@pccodigo'];
-			$mensajeSP[2] = $return['@pcid'];
+
 			
-			if($mensajeSP[1]==1){
-				header("Location: ../public/creacion-rutas.php?tour=$mensajeSP[2]");
- 
-			}else{
-				header("Location: ../public/creacion-tours.php?error=1");
+            //$sql_sp = "CALL SP_Tours('$datos[0]', '$datos[1]', $datos[2],'$datos[3]', 'agregar', @pcmensaje,@pccodigo,@pcid);";
+			// $ins_conexion = new Connection();
+			// $ins_conexion->connect();
+            // $ins_conexion->setConsulta($sql_sp);
+			// $qry = $ins_conexion->getResultConsulta();
+			// $return = $ins_conexion->getParametroSP("@pcmensaje,@pccodigo,@pcid");
+			// $mensajeSP[0] = $return['@pcmensaje'];
+			// $mensajeSP[1] = $return['@pccodigo'];
+			// $mensajeSP[2] = $return['@pcid'];
+
+			$connexionMysqli = new ConnexionMysqli();
+
+			$mysqli =  $connexionMysqli->connect();
+
+
+			$call = $mysqli->prepare('CALL SP_Tours(?, ? , ? , ? , "agregar",  @pcmensaje,@pccodigo,@pcid)');
+
+			$call->bind_param('ssis', 
+			$datos[0],
+			$datos[1],
+			$datos[2],
+			$datos[3]
+			);
+
+
+			$call->execute();
+					
+			$select = $mysqli->query('SELECT  @pcmensaje,@pccodigo,@pcid');
+					
+			$result = $select->fetch_assoc();
+			$codigo = $result['@pccodigo'];
+			$mensaje = $result['@pcmensaje'];
+			$id=$result['@pcid'];
+
+
+			
+			if($codigo[1]==1){
+
+				echo json_encode(
+					array(
+						"codigo"=>$codigo,
+						"mensaje"=>$mensaje,
+						"id"=>$id
+					)
+				);
 			}
+			else{
+				echo json_encode(
+					array(
+						"codigo"=>$codigo,
+						"mensaje"=>$mensaje,
+						"id"=>$id
+					)
+				);
+			}
+
+			$mysqli -> close();
+			
+			// if($mensajeSP[1]==1){
+			// 	header("Location: ../public/creacion-rutas.php?tour=$mensajeSP[2]");
+ 
+			// }else{
+			// 	header("Location: ../public/creacion-tours.php?error=1");
+			// }
 			
 			mysqli_close($conexion);
  ?>
