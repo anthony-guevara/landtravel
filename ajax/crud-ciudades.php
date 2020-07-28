5a2d5568b671872de70ac621f2da618f24c1b336
anthony-guevara <yalidanthony@gmail.com>
2020-07-27 19:05:26 -0600

    Crud ciudad backend

--- ajax/crud-ciudades.php
+++ ajax/crud-ciudades.php
@@ -9,7 +9,7 @@ $connexionMysqli = new ConnexionMysqli();
 $mysqli =  $connexionMysqli->connect();
 
 
-switch($_POST['accion']){
+switch ($_POST['accion']) {
 
 
     case "traerciudades":
@@ -19,7 +19,7 @@ switch($_POST['accion']){
 
         $call->execute();
         $result = $call->get_result();
-        //$destinos = $result->fetch_assoc(); 
+        //$destinos = $result->fetch_assoc();
         $destinos = $result->fetch_all(MYSQLI_ASSOC);
 
         echo json_encode($destinos);
@@ -28,23 +28,121 @@ switch($_POST['accion']){
     break;
 
     case "eliminarciudad":
-        echo "i es igual a 0";
+        $idciudad = (int)$_POST['idciudad'];
+
+        $call = $mysqli->prepare('UPDATE DESTINO  SET estado=0 WHERE id=?;');
+
+        $call->bind_param("i", $idciudad);
+        
+        $call->execute();
+
+        $filasAfectadas = $call->affected_rows;
+
+        if ($filasAfectadas>0) {
+            echo json_encode(
+                array(
+                    "codigo"=>1,
+                    "mensaje"=>"Se eliminó correctamente"
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    "codigo"=>0,
+                    "mensaje"=>"Ocurrió un error"
+                )
+            );
+        }
+
+
     break;
 
 
     case "editarciudad":
-        echo "i es igual a 0";
+        $idciudad = (int)$_POST['idciudad'];
+        $nombreCiudad = $_POST['nombreciudad'];
+        $descripcion  = $_POST['descripcion'];
+
+        $call = $mysqli->prepare("
+        UPDATE landtravel.destino SET  nombre=?,  descripcion=? WHERE id=?;");
+
+        $call->bind_param("ssi", $nombreCiudad, $descripcion, $idciudad);
+        
+        $call->execute();
+
+        $filasAfectadas = $call->affected_rows;
+
+        if ($filasAfectadas>0) {
+            echo json_encode(
+                array(
+                    "codigo"=>1,
+                    "mensaje"=>"Se editó correctamente"
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    "codigo"=>0,
+                    "mensaje"=>"Ocurrió un error"
+                )
+            );
+        }
+        
+    break;
 
 
-break;
+    case "agregarciudad":
+
+        $idpais = (int)$_POST['idpais'];
+        $nombreCiudad = $_POST['nombreciudad'];
+        $descripcion  = $_POST['descripcion'];
+
+
+        $query = "INSERT INTO destino ( pais_id, nombre, descripcion, estado) VALUES(?, ?, ?, 1);";
+
+
+        $call = $mysqli->prepare($query);
+
+        $call->bind_param("iss", $idpais, $nombreCiudad, $descripcion);
+        
+        $call->execute();
+
+        $filasAfectadas = $call->affected_rows;
+
+        if ($filasAfectadas>0) {
+            echo json_encode(
+                array(
+                    "codigo"=>1,
+                    "mensaje"=>"Se agregó correctamente"
+                )
+            );
+        } else {
+            echo json_encode(
+                array(
+                    "codigo"=>0,
+                    "mensaje"=>"Ocurrió un error"
+                )
+            );
+        }
+
+    break;
+
+    case "traerpaises":
+        $call = $mysqli->prepare('SELECT id, nombre FROM pais  WHERE estado = 1;');
+
+        $call->execute();
+        $result = $call->get_result();
+        //$destinos = $result->fetch_assoc();
+        $paises = $result->fetch_all(MYSQLI_ASSOC);
+
+        echo json_encode($paises);
+
+
+    break;
+
 
 
 }
 
 
-
-
 $mysqli -> close();
-
-
-?>
--- procedimientos/cambios_tablas.sql
+++ procedimientos/cambios_tablas.sql
@@ -2,3 +2,7 @@
 --tabla pais
 alter table pais add column estado int;
 update  pais set estado=1;
+
+--tabla destino
+alter table destino add column estado int;
+update  destino set estado=1;
