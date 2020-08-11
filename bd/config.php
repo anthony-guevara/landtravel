f6ed4a811743e348ae8836860b61d5f557704c1c
anthony-guevara <yalidanthony@gmail.com>
2020-08-10 22:55:06 -0600

    Backend crud-paises, credenciales de base en AWS

--- bd/config.php
+++ bd/config.php
@@ -1,21 +1,32 @@
 <?php
-abstract class Config {
+abstract class Config
+{
+
+    // base en amazon
+    // protected $server = 'landtravel.cnvuznrls2s2.us-east-1.rds.amazonaws.com:3306';
+    // protected $dataBase = 'landtravel';
+    // protected $user = 'admin';
+    // protected $pass = 'landtravel_unah_2020';
+
     protected $server = 'localhost:3306';
     protected $dataBase = 'landtravel';
     protected $user = 'root';
     protected $pass = '';
     
-    protected function getServer() {
+    protected function getServer()
+    {
         return $this->server;
     }
-    protected function getDataBase() {
+    protected function getDataBase()
+    {
         return $this->dataBase;
     }
-    protected function getUser() {
+    protected function getUser()
+    {
         return $this->user;
     }
-    protected function getPass() {
+    protected function getPass()
+    {
         return $this->pass;
     }
 }
-?>
