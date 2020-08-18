18dac5483428b85c5d0f23cbb92a084d559d050e
Sumachik <davidflores.irias@gmail.com>
2020-08-18 16:59:50 -0600

    modficando estilos de crud-lugares turisticos

--- public/lugaresturisticos.php
+++ public/lugaresturisticos.php
@@ -21,6 +21,7 @@ $conexion=$obj->conexion();
   <link rel="stylesheet" href="../vendors/owl-carousel/owl.carousel.min.css">
   <link rel="stylesheet" href="../vendors/flat-icon/font/flaticon.css">
   <link rel="stylesheet" href="../vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="../css/crud-tours-rutas.css">
 
 
   <!-- datatable -->
@@ -75,12 +76,14 @@ $conexion=$obj->conexion();
             <div class="container" style="text-align:center">
               <div class="row">
                 <div class="col col-sm-12">
-                  <h1 id="titulo"  class="colorletra" style="text-align: center; margin-top: 4px; margin-bottom:4rem;">Lugares Turisticos
+                <h1 id="titulos" style="text-align: center; margin-top: 3%; margin-bottom:2%;">Lugares Turísticos</h1>
 
-                    <button type="button" onclick="generarCiudades()" data-toggle="modal" data-target="#modalnuevolugar"
-                      class=" botonagregar" id="agregarlugar"><i class="fas fa-plus-circle"></i></button>
+                    <form class="form-inline" style="justify-content: flex-end; margin-bottom: 0px;margin-top:50px;">
+                        <button type="button" onclick="generarCiudades()" data-toggle="modal" data-target="#modalnuevolugar" class="btn btn-primary mb-2" style="background-color: #e65b02; border-color: #e65b02; margin-right: 15px; width: 198px; margin-bottom:0px !important;" data-toggle="modal" data-target="#modalAdd">Agregar &nbsp; <i class="fas fa-plus"></i></button>
+                    </form>
+
                   </h1>
-                  <div class="tableText col col-lg-12 col-md-12 col-sm-12">
+                  <div class="tableText col col-lg-12 col-md-12 col-sm-12" style="margin-top: 10px;">
                     <!-- <table class="table ">
                       <thead class="thead-dark">
 
@@ -97,8 +100,8 @@ $conexion=$obj->conexion();
                       </tbody>
                     </table> -->
 
-                    <table id="tabla-lugares" class="table table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
-                      <thead class="estilotarjeta">
+                    <table id="tabla-lugares" class="table bordeado table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
+                      <thead class="estilotarjeta" style="background: linear-gradient(90deg, rgba(230,91,2,1) 16%, rgba(180,70,0,1) 40%, rgba(94,37,0,1) 90%) !important;">
                         <tr>
                           <th>Ciudad</th>
                           <th>Nombre</th>
@@ -171,15 +174,61 @@ $conexion=$obj->conexion();
                   </button>
                 </div>
                 <div class="modal-body">
+                <div class="form-row">
 
-                  <form id="informacionnuevolugar">
+
+                                    <div class="form-row">
+                                    <div class="form-group col-md-4" id="seleccionciudad">
+                                      <label class="d-flex justify-content-start">Ciudad</label>
+                                      <select class="custom-select" id="listaciudades">
+                                        
+                                      </select>
+                                    </div>
+
+                                    <div class="form-group col-md-4">
+                                      <label class="d-flex justify-content-start">Nombre</label>
+                                      <input type="hidden" name="id" value="jeje">
+                                      <input class="form-control" id="nombrenuevolugar"  placeholder="nombre lugar turístico" >
+                                    </div>
+
+                                    <div class="form-group col-md-4">
+                                      <label class="d-flex justify-content-start">Descripcion</label>
+                                      <input type="hidden" name="id" value="jeje">
+                                      <input class="form-control" id="descripcionnueva"  placeholder="descripcion de lugar turistico" >
+                                    </div>
+
+                                    
+                                   
+
+                                    
+                                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                 <!-- <form id="informacionnuevolugar">
                     <div class="row">
                       <div class="col-6" id="seleccionciudad">
                         <label>Seleccione una ciudad</label> <select type="text" style="width:10em"
                           id="listaciudades"></select><br>
                       </div>
                       <div class="col-6">
-                        <input type="text" id="nombrenuevolugar" placeholder="Nombre lugar turistico"><br>
+                        <input type="text" class="form-control" id="nombrenuevolugar" placeholder="Nombre lugar turistico"><br>
                       </div>
                     </div>
                     <div class="row">
@@ -188,20 +237,17 @@ $conexion=$obj->conexion();
                       </div>
                     </div>
 
+                   <input type="text" id="idlugar" style="display:none;" placeholder="id">
 
-
-                    <input type="text" id="idlugar" style="display:none;" placeholder="id">
-
-
-
-
-                  </form>
+                  </form>  -->
 
                 </div>
-                <button type="button" class=" buttones" id="guardarnuevolugar" onclick="agregarLugar()">Guardar<i
+                <div class="modal-footer">
+                  <button type="button" class="btn org-btn" id="guardarnuevolugar" onclick="agregarLugar()">Guardar &nbsp<i
                     class="fas fa-save"></i></button>
-                <button type="button" class=" buttones" data-dismiss="modal" onclick="cerrar()">Cerrar<i
+                  <button type="button" class="btn org-btn-drk" data-dismiss="modal" onclick="cerrar()">Cerrar &nbsp<i
                     class="fas fa-window-close"></i></button>
+                </div>
               </div>
             </div>
           </div>
