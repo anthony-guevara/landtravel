8922242f7ba161be94ef82e06783bd5c4d980415
anthony-guevara <yalidanthony@gmail.com>
2020-08-22 16:31:49 -0600

    Agregar notificaciones push a Service Worker

--- public/app.js
+++ public/app.js
@@ -1,6 +1,43 @@
+let swRegistration = null;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("sw.js")
-    .then((reg) => console.log("service worker registered"))
-    .catch((err) => console.log("service worker not registered", err));
+    .then((reg) => {
+      console.log("service worker registrado.");
+
+      swRegistration = reg;
+
+      displayNotification();
+    })
+    .catch((err) => console.log("service worker no registrado", err));
+}
+
+function displayNotification() {
+  if (window.Notification && Notification.permission === "granted") {
+    notification();
+  }
+  // If the user hasn't told if he wants to be notified or not
+  // Note: because of Chrome, we are not sure the permission property
+  // is set, therefore it's unsafe to check for the "default" value.
+  else if (window.Notification && Notification.permission !== "denied") {
+    Notification.requestPermission((status) => {
+      if (status === "granted") {
+        notification();
+      } else {
+        console.log("No permitiste el acceso a notificaciones.");
+      }
+    });
+  } else {
+    // If the user refuses to get notified
+    console.log("No permitiste el acceso a notificaciones.");
+  }
+}
+
+function notification() {
+  const options = {
+    body: "Para reservar, por favor inicia sesión.",
+    icon: "../img/logo.png",
+  };
+  swRegistration.showNotification("¡Bienvenido a Landtravel!", options);
 }
