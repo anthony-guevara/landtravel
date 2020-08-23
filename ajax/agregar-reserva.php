4645926fca3dc26a4ee60867de22f9212d48cf1b
anthony-guevara <yalidanthony@gmail.com>
2020-08-22 22:25:34 -0600

    Cambios menores

--- ajax/agregar-reserva.php
+++ ajax/agregar-reserva.php
@@ -1,47 +1,47 @@
-<?php 
-error_reporting(0);
+<?php
+// error_reporting(0);
 include_once("../bd/config.php");
 include_once("../bd/conexion_mysqli.php");
 
-$datos=array((int)$_POST['usuario'], (int)$_POST['tarjeta'],$_POST['cvv'],$_POST['fechaexp'],(int)$_POST['nidtour'],(float)$_POST['prima']);
+$datos=array((int)$_POST['usuario'], (int)$_POST['tarjeta'],$_POST['cvv'],$_POST['fechaexp'],(int)$_POST['nidtour'],(double)$_POST['prima']);
 
-for($i=0;$i<count($datos);$i++){
-	if ($datos[$i]==""){
-		$datos[$i]=null;
-	}
+for ($i=0;$i<count($datos);$i++) {
+    if ($datos[$i]=="") {
+        $datos[$i]=null;
+    }
 };
 
-			$connexionMysqli = new ConnexionMysqli();
+            $connexionMysqli = new ConnexionMysqli();
 
-			$mysqli =  $connexionMysqli->connect();
+            $mysqli =  $connexionMysqli->connect();
 
-			
-			$call = $mysqli->prepare('CALL SP_Reserva(?, ? , ? , ? , ?, ? , @pmensaje, @pcodigo);');
+            
+            $call = $mysqli->prepare('CALL SP_Reserva(?, ? , ? , ? , ?, ? , @pmensaje, @pcodigo);');
 
-			$call->bind_param('iissid',
-			$datos[0],
-			$datos[1],
-			$datos[2],
-			$datos[3],
-			$datos[4],
-			$datos[5],
-			);
+            $call->bind_param(
+                'iissid',
+                $datos[0],
+                $datos[1],
+                $datos[2],
+                $datos[3],
+                $datos[4],
+                $datos[5],
+            );
 
-			
-			$call->execute();
-					
-			$select = $mysqli->query('SELECT  @pmensaje, @pcodigo');
-					
-			$result = $select->fetch_assoc();
+            
+            $call->execute();
+                    
+            $select = $mysqli->query('SELECT  @pmensaje, @pcodigo');
+                    
+            $result = $select->fetch_assoc();
 
-			$codigo = $result['@pcodigo'];
-			$mensaje = $result['@pmensaje'];
+            $codigo = $result['@pcodigo'];
+            $mensaje = $result['@pmensaje'];
 
-			echo json_encode(array(
-				$mensaje,
-				$codigo
-			));
+            echo json_encode(array(
+                $mensaje,
+                $codigo
+            ));
 
 
-			$mysqli -> close();
- ?>
+            $mysqli -> close();
